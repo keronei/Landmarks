9389961a75d0729f7aacc163766e38c9e8ea3065
keronei <keronei95@gmail.com>
2019-11-01 13:59:40 +0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Keronei on 01/11/2019.
+//  Copyright © 2019 Keronei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
